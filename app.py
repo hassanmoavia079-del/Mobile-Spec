05367f2506db6c017b813976d5fed61ab20b29b6
hassanmoavia079-del <hassanmoavia079@gmail.com>
2026-09-18 10:24:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ Rules:
 """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.6-flash",
         contents=prompt,
         config=types.GenerateContentConfig(
             response_mime_type="application/json"
